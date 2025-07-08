4e9782775e1cb8925baed3ddcbe34c3edaeda4e5
Jev <jev.kuznetsov@gmail.com>
2025-07-08 11:55:35 +0000

    Update development version to 0.25.0-dev

--- internal/version/version.go
+++ internal/version/version.go
@@ -12,7 +12,7 @@ import (
 var Version = "0.25.0"
 
 // DevVersion is the service current development version.
-var DevVersion = "0.25.0"
+var DevVersion = "0.25.0-dev"
 
 func GetCurrentVersion(mode string) string {
 	if mode == "dev" || mode == "demo" {
